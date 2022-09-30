46e2bc86a4436b3998a5ca5c21b3f99430716a46
Aliya N. A <aliya@agate.id>
2022-09-30 15:59:55 +0700

    Horizontal movement yeeehaaww

--- src/scenes/objects/Player.ts
+++ src/scenes/objects/Player.ts
@@ -1,14 +1,19 @@
 import Phaser from "phaser";
 import PlayerBullets from "./PlayerBullets";
-import { internalHeight } from "../../config";
+import { internalHeight, internalWidth } from "../../config";
 
 export default class Player extends Phaser.GameObjects.Sprite {
   scene: Phaser.Scene;
   physics: Phaser.Physics.Arcade.ArcadePhysics;
   upKey: Phaser.Input.Keyboard.Key;
   downKey: Phaser.Input.Keyboard.Key;
+  leftKey: Phaser.Input.Keyboard.Key;
+  rightKey: Phaser.Input.Keyboard.Key;
   shootKey: Phaser.Input.Keyboard.Key;
+
+  forwardSpeed: number;
   movementSpeed: number;
+
   declare body: Phaser.Physics.Arcade.Body;
 
   isInvincible: boolean;
@@ -31,8 +36,12 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.upKey = this.scene.input.keyboard.addKey("UP");
     this.downKey = this.scene.input.keyboard.addKey("DOWN");
     this.shootKey = this.scene.input.keyboard.addKey("Z");
+    this.rightKey = this.scene.input.keyboard.addKey("RIGHT");
+    this.leftKey = this.scene.input.keyboard.addKey("LEFT");
 
     this.body.setVelocityX(initForwardSpeed)
+
+    this.forwardSpeed = initForwardSpeed;
     this.movementSpeed = 200;
 
     this.isInvincible = false;
@@ -63,15 +72,29 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.bulletManager = bulletManager;
   }
 
+
+
   update() {
-    if (this.upKey.isDown) {
+    if (this.upKey.isDown && this.y > 0) {
       this.body.setVelocityY(-this.movementSpeed)
     }
-    else if (this.downKey.isDown) {
+    else if (this.downKey.isDown && this.y <= internalHeight) {
       this.body.setVelocityY(this.movementSpeed)
     }
     else {
       this.body.setVelocityY(0)
+    }
+
+    const scrollX = this.scene.cameras.main.scrollX;
+
+    if (this.leftKey.isDown && this.x > scrollX) {
+      this.body.setVelocityX(-this.movementSpeed);
+    }
+    else if(this.rightKey.isDown && this.x <= scrollX + internalWidth) {
+      this.body.setVelocityX(this.movementSpeed + this.forwardSpeed);
+    }
+    else {
+      this.body.setVelocityX(this.forwardSpeed);
     }
 
     if (this.isShooting && this.bulletManager) {
